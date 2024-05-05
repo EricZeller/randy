08c5c9bed66afef03b4a9d6a1c82b7cbe37c1497
EricZeller <98104805+EricZeller@users.noreply.github.com>
2024-05-05 21:06:39 +0200

    Initial commit

---
+++ app/src/main/java/de/ericz/tippy/MainActivity.kt
@@ -0,0 +1,46 @@
+package de.ericz.tippy
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun generateRandomNumber(view: View) {
+        val from = findViewById<EditText>(R.id.from)
+        val aFrom = from.text.toString()
+        val to = findViewById<EditText>(R.id.to)
+        val aTo = to.text.toString()
+
+        if (aFrom.isNotEmpty() && aTo.isNotEmpty()) {
+            val pFrom = aFrom.toInt()
+            val pTo = aTo.toInt()
+            val randomNumberTextView = findViewById<TextView>(R.id.textViewResult)
+            val randomNumber = (pFrom..pTo).random()
+            randomNumberTextView.text = "$randomNumber"
+            Toast.makeText(this, "Generated!", Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(this, "Please insert a valid number", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+}
