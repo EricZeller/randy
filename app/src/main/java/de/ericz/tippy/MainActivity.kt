ff1768ea860b76bedaa045d8c0419b7a60fcee55
EricZeller <98104805+EricZeller@users.noreply.github.com>
2024-05-07 20:24:55 +0200

    bug fixes

--- app/src/main/java/de/ericz/tippy/MainActivity.kt
+++ app/src/main/java/de/ericz/tippy/MainActivity.kt
@@ -2,6 +2,7 @@ package de.ericz.tippy
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -27,20 +28,23 @@ class MainActivity : AppCompatActivity() {
         val aFrom = from.text.toString()
         val to = findViewById<EditText>(R.id.to)
         val aTo = to.text.toString()
+        var firstGeneration = true
 
-        if (aFrom.isNotEmpty() && aTo.isNotEmpty()) {
+        if (aFrom.isNotEmpty() && aTo.isNotEmpty() && (aFrom <= aTo)) {
             val pFrom = aFrom.toInt()
             val pTo = aTo.toInt()
             val randomNumberTextView = findViewById<TextView>(R.id.textViewResult)
             val randomNumber = (pFrom..pTo).random()
             randomNumberTextView.text = "$randomNumber"
             Toast.makeText(this, "Generated!", Toast.LENGTH_SHORT).show()
-
+            firstGeneration = false
         } else {
             Toast.makeText(this, "Please insert a valid number", Toast.LENGTH_SHORT).show()
         }
 
-
+        if (!firstGeneration) {
+            findViewById<Button>(R.id.buttonNew).text = "New one"
+        }
 
     }
 }
